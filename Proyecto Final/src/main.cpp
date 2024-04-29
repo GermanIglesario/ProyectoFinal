1e82b49c8ffbaf8a189e89bc94de7527d95a8372
German Iglesario <166442593+GermanIglesario@users.noreply.github.com>
2024-04-28 23:03:18 -0300

    Update main.cpp
    Ultimo agregado del dia

--- Proyecto Final/src/main.cpp
+++ Proyecto Final/src/main.cpp
@@ -1,13 +1,22 @@
 #include <Arduino.h>
+#include <string.h>
 #include <GCodeParser.h>
 #include <Stepper.h>
 #include <LiquidCrystal.h>
 #include <TimerOne.h>
 #include <SD.h>
 
+//--------------------------------------Inicializaciones de librerias--------------------------------------
+
 File archivo;
 
 LiquidCrystal LCD(12,11,10,9,8,7); // Pines del LCD 20x4
+
+//--------------------------------------Declaraciones e inicializaciones de variables--------------------------------------
+
+int ultimaPosicionSD = 0;
+
+//--------------------------------------Setup--------------------------------------
 
 void setup() {
 
@@ -18,10 +27,24 @@ void setup() {
 
 }
 
+//--------------------------------------Loop--------------------------------------
+
 void loop() {
   
-  archivo = SD.open("nombre_archivo.txt"); // Se pasa el archivo al creado dentro del programa para poder trabajarlo
+  lectura_SD();
+
 }
+
+//--------------------------------------Funciones--------------------------------------
+
+char lectura_SD(){
+
+  archivo = SD.open("nombre_archivo.txt", FILE_READ); // Se pasa el archivo al creado dentro del programa para poder trabajarlo
+
+
+}
+
+//--------------------------------------Timer--------------------------------------
 
 void timer(){
 
