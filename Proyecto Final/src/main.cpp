5905766ef68b6909432ec8f03075a7897d76875b
German Iglesario <166442593+GermanIglesario@users.noreply.github.com>
2024-05-08 17:36:03 -0300

    Update main.cpp

--- Proyecto Final/src/main.cpp
+++ Proyecto Final/src/main.cpp
@@ -25,8 +25,11 @@ int DT = 3; // Señal B del Encoder
 int posicionAntes; // Variable Encoder
 int posicionAhora; // Variable Encoder
 int pulsEncoder = 0; // Pulsador Encoder
-int estadoSwitch = 0;
+int estadoSwitch = 0; // Estado del switch segun el encoder
+int estadoPrevioSwitch; // Estado previo del switch segun el encoder
 int e = 1; // Variable de unica vez, pantalla de inicio
+int a1 = 1; // Variable de unica vez, limpiar la pantalla
+int a2 = 1; // Variable de unica vez, limpiar la pantalla
 
 //--------------------------------------Setup--------------------------------------
 
@@ -59,7 +62,7 @@ void loop() {
       LCD.setCursor(1,3);
       LCD.print("Fefi's production");
 
-      delay(2000);
+      delay(3000);
 
       LCD.clear();
       LCD.setCursor(0,0);
@@ -71,7 +74,7 @@ void loop() {
       LCD.setCursor(0,3);
       LCD.print("-Lentini");
 
-      delay(2000);
+      delay(3000);
     
       LCD.clear();
       estadoSwitch = 1;
@@ -83,18 +86,28 @@ void loop() {
 
   case 1: // Menu principal
 
+    if(estadoPrevioSwitch == 2){
+      estadoSwitch = 2;
+      break;
+    }
     e = 0;
+    if(a1){
+      LCD.clear();
+      a1 = 0;
+      a2 = 1;
+    }
+    LCD.noCursor();
     delay(1000);
     LCD.setCursor(0,0);
     LCD.print("T:");
     //
     // Pantalla principal (pendiente)
     //
-    while(digitalRead(pulsEncoder) == LOW){
+    while(1){ // Función que utiliza el encoder
       estadoSwitch = 1;
-      delay(1);
       if(digitalRead(pulsEncoder) == HIGH){
         estadoSwitch = 2;
+        break;
       }
     }
 
@@ -102,21 +115,265 @@ void loop() {
   
   case 2: // Opciones generales
 
+    if(a2){
+      LCD.clear();
+      a1 = 1;
+      a2 = 0;
+    }
+    LCD.setCursor(0,0);
+    LCD.print("- Volver al menu");
+    LCD.setCursor(0,1);
+    LCD.print("- Empezar grabado");
+    LCD.setCursor(0,2);
+    LCD.print("- Pausar grabado");
+    LCD.setCursor(0,3);
+    LCD.print("- Terminar grabado");
 
-    LCD.setCursor(20,0);
+    LCD.setCursor(16,0);
     LCD.cursor();
-    
+    estadoPrevioSwitch = 2;
+
+    while(1){ // Función "volver al menu"
+      if(digitalRead(pulsEncoder) == HIGH){ 
+        estadoSwitch = 1;
+        break;
+      }
+    }
+
     break;
+
+  case 3:
+
+    if(a1){
+      LCD.clear();
+      a1 = 0;
+      a2 = 1;
+    }
+    LCD.setCursor(0,0);
+    LCD.print("- Volver al menu");
+    LCD.setCursor(0,1);
+    LCD.print("- Empezar grabado");
+    LCD.setCursor(0,2);
+    LCD.print("- Pausar grabado");
+    LCD.setCursor(0,3);
+    LCD.print("- Terminar grabado");
+
+    LCD.setCursor(17,1);
+    LCD.cursor();
+
+    while(1){ // Función para selec. "empezar grabado"
+      if(digitalRead(pulsEncoder) == HIGH){
+        estadoSwitch = NULL; // Falta crear la función para empezar el grabado
+        break;
+      }
+    }
+
+    break;
+  
+  case 4:
+
+    if(a2){
+      LCD.clear();
+      a1 = 1;
+      a2 = 0;
+    }
+    LCD.setCursor(0,0);
+    LCD.print("- Volver al menu");
+    LCD.setCursor(0,1);
+    LCD.print("- Empezar grabado");
+    LCD.setCursor(0,2);
+    LCD.print("- Pausar grabado");
+    LCD.setCursor(0,3);
+    LCD.print("- Terminar grabado");
+
+    LCD.setCursor(16,2);
+    LCD.cursor();
+
+    while(1){ // Función para selec. "pausar grabado"
+      if(digitalRead(pulsEncoder) == HIGH){
+        estadoSwitch = NULL; // Falta crear la función para pausar el grabado
+        break;
+      }
+    }
+
+    break;
+  
+  case 5:
+
+    if(estadoPrevioSwitch == 6){
+      estadoSwitch = 10;
+      break;
+    }
+    if(a1){
+      LCD.clear();
+      a1 = 0;
+      a2 = 1;
+    }
+    LCD.setCursor(0,0);
+    LCD.print("- Volver al menu");
+    LCD.setCursor(0,1);
+    LCD.print("- Empezar grabado");
+    LCD.setCursor(0,2);
+    LCD.print("- Pausar grabado");
+    LCD.setCursor(0,3);
+    LCD.print("- Terminar grabado");
+
+    LCD.setCursor(18,3);
+    LCD.cursor();
+
+    while(1){ // Función para selec. "terminar grabado"
+      if(digitalRead(pulsEncoder) == HIGH){
+        estadoSwitch = NULL; // Falta crear la función para terminar el grabado
+        break;
+      }
+    }
+
+    break;
+  
+  case 6:
+
+    if(a2){
+      LCD.clear();
+      a1 = 1;
+      a2 = 0;
+    }
+    
+    LCD.setCursor(0,0);
+    LCD.print("- Empezar grabado");
+    LCD.setCursor(0,1);
+    LCD.print("- Pausar grabado");
+    LCD.setCursor(0,2);
+    LCD.print("- Terminar grabado");
+    LCD.setCursor(0,3);
+    LCD.print("- Info del producto");
+
+    LCD.setCursor(19,3);
+    LCD.cursor();
+    estadoPrevioSwitch = 6;
+
+    while(1){ // Función para selec. "info del producto"
+      if(digitalRead(pulsEncoder) == HIGH){
+        estadoSwitch = NULL; // Falta crear la función para ver la información del producto
+        break;
+      }
+    }
+
+    break;
+
+  case 7:
+
+    if(estadoPrevioSwitch == 6){
+      estadoSwitch = 6;
+      break;
+    }
+    if(estadoPrevioSwitch == 8){
+      estadoSwitch = 2;
+      break;
+    }
+
+    break;
+
+  case 8:
+
+    if(a1){
+      LCD.clear();
+      a1 = 0;
+      a2 = 1;
+    }
+    LCD.setCursor(0,0);
+    LCD.print("- Empezar grabado");
+    LCD.setCursor(0,1);
+    LCD.print("- Pausar grabado");
+    LCD.setCursor(0,2);
+    LCD.print("- Terminar grabado");
+    LCD.setCursor(0,3);
+    LCD.print("- Info del producto");
+
+    LCD.setCursor(17,0);
+    LCD.cursor();
+    estadoPrevioSwitch = 8;
+
+    while(1){ // Función para selec. "empezar grabado"
+      if(digitalRead(pulsEncoder) == HIGH){
+        estadoSwitch = NULL; // Falta crear la función para empezar el grabado
+        break;
+      }
+    }
+
+    break;
+
+  case 9:
+
+    if(a2){
+      LCD.clear();
+      a1 = 1;
+      a2 = 0;
+    }
+    
+    LCD.setCursor(0,0);
+    LCD.print("- Empezar grabado");
+    LCD.setCursor(0,1);
+    LCD.print("- Pausar grabado");
+    LCD.setCursor(0,2);
+    LCD.print("- Terminar grabado");
+    LCD.setCursor(0,3);
+    LCD.print("- Info del producto");
+
+    LCD.setCursor(16,1);
+    LCD.cursor();
+
+    while(1){ // Función para selec. "pausar grabado"
+      if(digitalRead(pulsEncoder) == HIGH){
+        estadoSwitch = NULL; // Falta crear la función para pausar el grabado
+        break;
+      }
+    }
+
+    break;
+
+  case 10:
+
+    if(a1){
+      LCD.clear();
+      a1 = 0;
+      a2 = 1;
+    }
+    
+    LCD.setCursor(0,0);
+    LCD.print("- Empezar grabado");
+    LCD.setCursor(0,1);
+    LCD.print("- Pausar grabado");
+    LCD.setCursor(0,2);
+    LCD.print("- Terminar grabado");
+    LCD.setCursor(0,3);
+    LCD.print("- Info del producto");
+
+    LCD.setCursor(18,2);
+    LCD.cursor();
+    estadoPrevioSwitch = 10;
+
+    while(1){ // Función para selec. "terminar grabado"
+      if(digitalRead(pulsEncoder) == HIGH){
+        estadoSwitch = NULL; // Falta crear la función para terminar el grabado
+        break;
+      }
+    }
+
+    break;
+
+  case 11:
+
+    if(estadoPrevioSwitch == 10){
+      estadoSwitch = 6;
+    }
+
+    break;
+  
   }
+
   // Habra tantos casos como pantallas de opciones haya, estas pantallas se generan por cualquier cambio hecho
   // Los casos tienen que poder repetirse unicamente desde el 1 hasta el numero que se llegue
   // El caso 0 es unicamente el comienzo visual del programa
-
-  //-----------------------------------Llamados-----------------------------------
-  
-  lectura();
-  interpretacion();
-  movimiento_PaP();
 
 }
 
