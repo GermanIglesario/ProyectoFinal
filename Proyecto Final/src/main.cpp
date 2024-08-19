14c1fb07f9a37c930f1b9d9e300d1d1c43d11177
German Iglesario <166442593+GermanIglesario@users.noreply.github.com>
2024-08-19 18:21:49 -0300

    Update no a los delays parte 1.cpp
    Eliminados los delays de parte del menu y de la función de los pulsadores.

--- Proyecto Final/src/main.cpp
+++ Proyecto Final/src/main.cpp
@@ -16,9 +16,7 @@
 
 ////--------------------------------------Inicializaciones de librerias--------------------------------------
 
-const int pasosPorRevolucion = 200; // Segun los grados
-Stepper myStepper1(pasosPorRevolucion, 9, 10, 11, 12);
-Stepper myStepper2(pasosPorRevolucion, 5, 6, 7, 8);
+// Paso a paso???
 
 File archivo;
 File archivos[4];
@@ -45,7 +43,6 @@ int coordenadaZ1;
 
 float finalX; // Variable de la coordenada X
 float finalY; // Variable de la coordenada Y
-float finalZ; // Variable de la coordenada Z
 int RPM = 60; // Revoluciones por minuto del motor paso a paso
 int posC; // Posición para guardar coordenadas
 
@@ -62,6 +59,34 @@ int e = 1; // Variable de unica vez, pantalla de inicio
 int a1 = 1; // Variable de unica vez, limpiar la pantalla
 int a2 = 1; // Variable de unica vez, limpiar la pantalla
 
+//--------------------------------------Variables Timer--------------------------------------
+
+int mil1 = 0; // Variable para evitar el uso de delay() en movimiento_Pulsadores;
+int mil2 = 0;
+int mil3 = 0;
+int mil4 = 0;
+int mil5 = 0;
+int mil6 = 0;
+int mil7 = 0;
+int mil8 = 0;
+int mil9 = 0;
+int mil10 = 0;
+int mil11 = 0;
+int mil12 = 0;
+int mil13 = 0;
+int mil14 = 0;
+int mil15 = 0;
+int mil16 = 0;
+int mil17 = 0;
+int mil18 = 0;
+int mil19 = 0;
+int mil20 = 0;
+int mil21 = 0;
+int mil22 = 0;
+int mil23 = 0;
+int mil24 = 0;
+int mil25 = 0;
+
 //--------------------------------------Menu Principal--------------------------------------
 
 int temperatura; // Temperatura de la punta
@@ -70,7 +95,7 @@ int tiempo; // Tiempo que lleva el grabado
 
 ////--------------------------------------Prototipado Funciones--------------------------------------
 
-void movimientoPulsadores();
+void movimiento_Pulsadores();
 void deteccion_Archivos();
 void lectura_SD();
 void interpretacion_SD();
@@ -96,8 +121,6 @@ void setup() {
   pinMode(puls1, INPUT); // Pulsadores
   pinMode(puls2, INPUT);
   pinMode(puls3, INPUT);
-  myStepper1.setSpeed(50); // Stepper 1
-  myStepper2.setSpeed(50); // Stepper 2
 
 }
 
@@ -112,7 +135,7 @@ void loop() {
   // Caso 2 al 10: Menu de Opciones
   // Caso 12 al 15: Opcion "Info del Producto"
   // Caso 17 al 18: Selección de Archivo
-  // Caso 47 al ???: Extensión del Menu de Opciones
+  // Caso 47 al 55: Extensión del Menu de Opciones
   
   switch (estadoSwitch){ // Switch del menu visual
 
@@ -159,21 +182,20 @@ void loop() {
       a2 = 1;
     }
     LCD.noCursor();
-    delay(500);
     LCD.setCursor(3,1);
     LCD.print("Menu Principal");
     LCD.setCursor(5,2);
     LCD.print("en proceso");
+
     //
     // Pantalla principal (pendiente)
     //
-    while(1){ // Permanencia en el menu
-      estadoSwitch = 1;
+
+    if(mil2 >= 200){
       if(digitalRead(puls1) == HIGH){
         estadoSwitch = 2;
-        delay(250);
-        break;
       }
+      mil2 = 0;
     }
 
     break;
@@ -198,11 +220,13 @@ void loop() {
     LCD.print("*");
     estadoPrevioSwitch = 2;
 
-    movimientoPulsadores();
-    if(digitalRead(puls1) == HIGH){
-      estadoPrevioSwitch = 0;
-      estadoSwitch = 1;
-      delay(250);
+    movimiento_Pulsadores();
+    if(mil3 >= 200){
+      if(digitalRead(puls1) == HIGH){
+        estadoPrevioSwitch = 0;
+        estadoSwitch = 1;
+      }
+      mil3 = 0;
     }
 
     break;
@@ -226,11 +250,14 @@ void loop() {
     LCD.setCursor(17,1);
     LCD.print("*");
 
-    movimientoPulsadores();
-    if(digitalRead(puls1) == HIGH){ // Función para selec. "empezar grabado"
-      estadoSwitch = 17;
-      delay(250);
+    movimiento_Pulsadores();
+    if(mil4 >= 200){
+      if(digitalRead(puls1) == HIGH){ // Función para selec. "empezar grabado"
+        estadoSwitch = 17;
+      }
+      mil4 = 0;
     }
+    
 
     break;
   
@@ -253,10 +280,14 @@ void loop() {
     LCD.setCursor(16,2);
     LCD.print("*");
 
-    movimientoPulsadores();
-    if(digitalRead(puls1) == HIGH){ // Función para selec. "pausar grabado"
-      estadoSwitch = NULL;
+    movimiento_Pulsadores();
+    if(mil5 >= 200){
+      if(digitalRead(puls1) == HIGH){ // Función para selec. "pausar grabado"
+        estadoSwitch = NULL;
+      }
+      mil5 = 0;
     }
+    
     
     break;
   
@@ -282,10 +313,14 @@ void loop() {
     LCD.setCursor(18,3);
     LCD.print("*");
 
-    movimientoPulsadores();
-    if(digitalRead(puls1) == HIGH){ // Función para selec. "terminar grabado"
-      estadoSwitch = NULL;
+    movimiento_Pulsadores();
+    if(mil6 >= 200){
+      if(digitalRead(puls1) == HIGH){ // Función para selec. "terminar grabado"
+        estadoSwitch = NULL;
+      }
+      mil6 = 0;
     }
+    
     
     break;
   
@@ -310,9 +345,12 @@ void loop() {
     LCD.print("*");
     estadoPrevioSwitch = 6;
 
-    movimientoPulsadores();
-    if(digitalRead(puls1) == HIGH){ // Función para selec. "coord. manuales"
-      estadoSwitch = NULL;
+    movimiento_Pulsadores();
+    if(mil7 >= 200){
+      if(digitalRead(puls1) == HIGH){ // Función para selec. "coord. manuales"
+        estadoSwitch = NULL;
+      }
+      mil7 = 0;
     }
     
     break;
@@ -348,9 +386,12 @@ void loop() {
     LCD.print("*");
     estadoPrevioSwitch = 8;
 
-    movimientoPulsadores();
-    if(digitalRead(puls1) == HIGH){ // Función para selec. "empezar grabado"
-      estadoSwitch = NULL;
+    movimiento_Pulsadores();
+    if(mil8 >= 200){
+      if(digitalRead(puls1) == HIGH){ // Función para selec. "empezar grabado"
+        estadoSwitch = NULL;
+      }
+      mil8 = 0;
     }
     
     break;
@@ -375,9 +416,12 @@ void loop() {
     LCD.setCursor(16,1);
     LCD.print("*");
 
-    movimientoPulsadores();
-    if(digitalRead(puls1) == HIGH){ // Función para selec. "pausar grabado"
-      estadoSwitch = NULL;
+    movimiento_Pulsadores();
+    if(mil9 >= 200){
+      if(digitalRead(puls1) == HIGH){ // Función para selec. "pausar grabado"
+        estadoSwitch = NULL;
+      }
+      mil9 = 0;
     }
     
     break;
@@ -403,9 +447,12 @@ void loop() {
     LCD.print("*");
     estadoPrevioSwitch = 10;
 
-    movimientoPulsadores();
-    if(digitalRead(puls1) == HIGH){ // Función para selec. "terminar grabado"
-      estadoSwitch = NULL;
+    movimiento_Pulsadores();
+    if(mil10 >= 200){
+      if(digitalRead(puls1) == HIGH){ // Función para selec. "terminar grabado"
+        estadoSwitch = NULL;
+      }
+      mil10 = 0;
     }
     
     break;
@@ -439,10 +486,12 @@ void loop() {
     estadoPrevioSwitch = 12;
 
 
-    movimientoPulsadores();
-    if(digitalRead(puls1) == HIGH){
-      estadoSwitch = 1;
-      delay(250);
+    movimiento_Pulsadores();
+    if(mil21 >= 200){
+      if(digitalRead(puls1) == HIGH){
+        estadoSwitch = 1;
+      }
+      mil21 = 0;
     }
 
     break;
@@ -465,11 +514,13 @@ void loop() {
     LCD.print("- Lentini");
     estadoPrevioSwitch = 13;
 
-    movimientoPulsadores();
-    if(digitalRead(puls1) == HIGH){
-      a1 = 1, a2 = 0;
-      estadoSwitch = 1;
-      delay(250);
+    movimiento_Pulsadores();
+    if(mil22 >= 200){
+      if(digitalRead(puls1) == HIGH){
+        a1 = 0, a2 = 0;
+        estadoSwitch = 1;
+      }
+      mil22 = 0;
     }
 
     break;
@@ -514,7 +565,7 @@ void loop() {
     */
     estadoPrevioSwitch = 17;
 
-    movimientoPulsadores();
+    movimiento_Pulsadores();
 
     break;
 
@@ -556,13 +607,13 @@ void loop() {
     LCD.setCursor(0,2);
     LCD.print("- Coord. manuales");
     LCD.setCursor(0,3);
-    LCD.print("- Info del producto");
+    LCD.print("- Retorno al home");
 
     LCD.setCursor(16,0);
     LCD.print("*");
     estadoPrevioSwitch = 47;
 
-    movimientoPulsadores();
+    movimiento_Pulsadores();
     if(digitalRead(puls1) == HIGH){ // Función para selec. "pausar grabado"
       estadoSwitch = NULL;
     }
@@ -584,12 +635,12 @@ void loop() {
     LCD.setCursor(0,2);
     LCD.print("- Coord. manuales");
     LCD.setCursor(0,3);
-    LCD.print("- Info del producto");
+    LCD.print("- Retorno al home");
 
     LCD.setCursor(18,1);
     LCD.print("*");
 
-    movimientoPulsadores();
+    movimiento_Pulsadores();
     if(digitalRead(puls1) == HIGH){ // Función para selec. "terminar grabado"
       estadoSwitch = NULL;
     }
@@ -611,12 +662,12 @@ void loop() {
     LCD.setCursor(0,2);
     LCD.print("- Coord. manuales");
     LCD.setCursor(0,3);
-    LCD.print("- Info del producto");
+    LCD.print("- Retorno al home");
 
     LCD.setCursor(17,2);
     LCD.print("*");
 
-    movimientoPulsadores();
+    movimiento_Pulsadores();
     if(digitalRead(puls1) == HIGH){ // Función para selec. "coord. manuales"
       estadoSwitch = NULL;
     }
@@ -638,24 +689,145 @@ void loop() {
     LCD.setCursor(0,2);
     LCD.print("- Coord. manuales");
     LCD.setCursor(0,3);
-    LCD.print("- Info del producto");
+    LCD.print("- Retorno al home");
 
-    LCD.setCursor(19,3);
+    LCD.setCursor(17,3);
     LCD.print("*");
     estadoPrevioSwitch = 50;
 
-    movimientoPulsadores();
-    if(digitalRead(puls1) == HIGH){ // Función para selec. "info del producto"
-      estadoSwitch = 12;
-      delay(250);
+    movimiento_Pulsadores();
+    if(digitalRead(puls1) == HIGH){ // Función para selec. "retorno al home"
+      estadoSwitch = NULL;
     }
 
     break;
 
   case 51:
-
+  
     if(estadoPrevioSwitch == 50){
-      estadoSwitch = 50;
+      estadoSwitch = 55;
+    }
+
+    if (estadoPrevioSwitch == 52){
+      estadoSwitch = 47;
+    }
+    
+    break;
+
+  case 52:
+
+    if(a1){
+      LCD.clear();
+      a1 = 0;
+      a2 = 1;
+    }
+    
+    LCD.setCursor(0,0);
+    LCD.print("- Terminar grabado");
+    LCD.setCursor(0,1);
+    LCD.print("- Coord. manuales");
+    LCD.setCursor(0,2);
+    LCD.print("- Retorno al home");
+    LCD.setCursor(0,3);
+    LCD.print("- Info del producto");
+
+    LCD.setCursor(18,0);
+    LCD.print("*");
+    estadoPrevioSwitch = 52;
+
+    movimiento_Pulsadores();
+    if(digitalRead(puls1) == HIGH){ // Función para selec. "terminar grabado"
+      estadoSwitch = NULL;
+    }
+
+    break;
+
+  case 53:
+
+    if(a2){
+      LCD.clear();
+      a1 = 1;
+      a2 = 0;
+    }
+    
+    LCD.setCursor(0,0);
+    LCD.print("- Terminar grabado");
+    LCD.setCursor(0,1);
+    LCD.print("- Coord. manuales");
+    LCD.setCursor(0,2);
+    LCD.print("- Retorno al home");
+    LCD.setCursor(0,3);
+    LCD.print("- Info del producto");
+
+    LCD.setCursor(17,1);
+    LCD.print("*");
+
+    movimiento_Pulsadores();
+    if(digitalRead(puls1) == HIGH){ // Función para selec. "coord. manuales"
+      estadoSwitch = NULL;
+    }
+
+    break;
+  
+  case 54:
+
+    if(a1){
+      LCD.clear();
+      a1 = 0;
+      a2 = 1;
+    }
+    
+    LCD.setCursor(0,0);
+    LCD.print("- Terminar grabado");
+    LCD.setCursor(0,1);
+    LCD.print("- Coord. manuales");
+    LCD.setCursor(0,2);
+    LCD.print("- Retorno al home");
+    LCD.setCursor(0,3);
+    LCD.print("- Info del producto");
+
+    LCD.setCursor(17,2);
+    LCD.print("*");
+
+    movimiento_Pulsadores();
+    if(digitalRead(puls1) == HIGH){ // Función para selec. "retorno al home"
+      estadoSwitch = NULL;
+    }
+
+    break;
+
+  case 55:
+
+    if(a2){
+      LCD.clear();
+      a1 = 1;
+      a2 = 0;
+    }
+    
+    LCD.setCursor(0,0);
+    LCD.print("- Terminar grabado");
+    LCD.setCursor(0,1);
+    LCD.print("- Coord. manuales");
+    LCD.setCursor(0,2);
+    LCD.print("- Retorno al home");
+    LCD.setCursor(0,3);
+    LCD.print("- Info del producto");
+
+    LCD.setCursor(19,3);
+    LCD.print("*");
+    estadoPrevioSwitch = 55;
+
+    movimiento_Pulsadores();
+    if(digitalRead(puls1) == HIGH){ // Función para selec. "info del producto"
+      estadoSwitch = 12;
+    }
+
+    break;
+
+  case 56:
+
+    if (estadoPrevioSwitch == 55){
+      estadoSwitch = 55;
     }
 
     break;
@@ -670,18 +842,20 @@ void loop() {
 
 ////--------------------------------------Función Pulsadores--------------------------------------
 
-void movimientoPulsadores(){
+void movimiento_Pulsadores(){
 
-  if(digitalRead(puls2) == HIGH){
-    estadoSwitch++;
-    delay(200);
-  }
-  if(digitalRead(puls3) == HIGH){
-    estadoSwitch--;
-    delay(200);
+  if(mil1 >= 50){
+    if(digitalRead(puls2) == HIGH){
+      estadoSwitch++;
+    }
+    if(digitalRead(puls3) == HIGH){
+      estadoSwitch--;
+    }
+    mil1 = 0;
   }
 
 }
+  
 
 ////--------------------------------------Funciones Lectura e Interpretación--------------------------------------
 
@@ -718,7 +892,7 @@ void lectura_SD(String nombre){ // La función recibe como parametro el nombre d
   if(archivo){ // Si existe
     if(ultimaPosicion <= bytesTotales){ // Si no se termino de recorrer
       if(archivo.available()){ // Si esta disponible
-        for(int i = 0 ; archivo.read() != 10; i++, ultimaPosicion++){ // El 10 es el enter en tabla ASCII
+        for(int i = 0 ; cadena[i] != 10; i++, ultimaPosicion++){ // El 10 es el enter en tabla ASCII
           archivo.seek(ultimaPosicion); // Se ubica en el archivo
           cadena[i] = archivo.read(); // Lo lee y guarda
         }
@@ -742,9 +916,6 @@ void interpretacion_SD(){ // La función que interpreta el archivo gcode posteri
     if(cadena[i] >= '0' && cadena[i] <= '9' && anteriorCaracter == 'Y'){ // Un if para guardar Y
       coordenadaY(i);
     }
-    if(cadena[i] >= '0' && cadena[i] <= '9' && anteriorCaracter == 'Z'){ // Un if para guardar Z
-      coordenadaZ(i);
-    }
     anteriorCaracter = cadena[i - 1];
   }
 
@@ -753,7 +924,7 @@ void interpretacion_SD(){ // La función que interpreta el archivo gcode posteri
 void coordenadaX(int posC){ // Función para almacenar la coordenada X
 
   // Guardado de la parte entera
-  int aux1, aux2, aux3, aux4;
+  int aux1, aux2, aux3, aux4, aux5, aux6;
   for(; 1; posC++){
     aux1 = aux2;
     aux2 = aux3;
@@ -767,34 +938,28 @@ void coordenadaX(int posC){ // Función para almacenar la coordenada X
   finalX = aux1 * 100 + aux2 * 10 + aux3 * 1;
 
   // Guardado de la parte decimal
-  aux1 = 0, aux2 = 0, aux3 = 0, aux4 = 0;
+  aux1 = 0, aux2 = 0, aux3 = 0, aux4 = 0, aux5 = 0, aux6 = 0;
   int posC2 = posC + 1, i = 0;
   for(; 1; posC2++, i++){
     aux1 = aux2;
     aux2 = aux3;
-    aux3 = aux4;  
+    aux3 = aux4;
+    aux4 = aux5;
+    aux5 = aux6;
     if(cadena[posC2] == ' '){
       break;
     }
-    aux4 = cadena[posC2] - '0';
+    aux6 = cadena[posC2] - '0';
   }
 
-  if(i == 1){
-    finalX += aux3 * 0.1;
-  }
-  if(i == 2){
-    finalX += aux2 * 0.1 + aux3 * 0.01;
-  }
-  if(i == 3){
-    finalX += aux1 * 0.1 + aux2 * 0.01 + aux3 * 0.001;
-  }
+  finalX += aux1 * 0.1 +  aux2 * 0.01 +  aux3 * 0.001 +  aux4 * 0.0001 +  aux5 * 0.00001;
 
 }
 
 void coordenadaY(int posC){ // Función para almacenar la coordenada Y
 
   // Guardado de la parte entera
-  int aux1, aux2, aux3, aux4;
+  int aux1, aux2, aux3, aux4, aux5, aux6;
   for(; 1; posC++){
     aux1 = aux2;
     aux2 = aux3;
@@ -808,68 +973,21 @@ void coordenadaY(int posC){ // Función para almacenar la coordenada Y
   finalY = aux1 * 100 + aux2 * 10 + aux3 * 1;
 
   // Guardado de la parte decimal
-  aux1 = 0, aux2 = 0, aux3 = 0, aux4 = 0;
+  aux1 = 0, aux2 = 0, aux3 = 0, aux4 = 0, aux5 = 0, aux6 = 0;
   int posC2 = posC + 1, i = 0;
   for(; 1; posC2++, i++){
     aux1 = aux2;
     aux2 = aux3;
-    aux3 = aux4;  
+    aux3 = aux4;
+    aux4 = aux5;
+    aux5 = aux6;
     if(cadena[posC2] == ' '){
       break;
     }
-    aux4 = cadena[posC2] - '0';
+    aux6 = cadena[posC2] - '0';
   }
 
-  if(i == 1){
-    finalY += aux3 * 0.1;
-  }
-  if(i == 2){
-    finalY += aux2 * 0.1 + aux3 * 0.01;
-  }
-  if(i == 3){
-    finalY += aux1 * 0.1 + aux2 * 0.01 + aux3 * 0.001;
-  }
-
-}
-
-void coordenadaZ(int posC){ // Función para almacenar la coordenada Z
-
-// Guardado de la parte entera
-  int aux1, aux2, aux3, aux4;
-  for(; 1; posC++){
-    aux1 = aux2;
-    aux2 = aux3;
-    aux3 = aux4;  
-    if(cadena[posC] == '.'){
-      break;
-    }
-    aux4 = cadena[posC] - '0';
-  }
-  
-  finalZ = aux1 * 100 + aux2 * 10 + aux3 * 1;
-
-  // Guardado de la parte decimal
-  aux1 = 0, aux2 = 0, aux3 = 0, aux4 = 0;
-  int posC2 = posC + 1, i = 0;
-  for(; 1; posC2++, i++){
-    aux1 = aux2;
-    aux2 = aux3;
-    aux3 = aux4;  
-    if(cadena[posC2] == ' '){
-      break;
-    }
-    aux4 = cadena[posC2] - '0';
-  }
-
-  if(i == 1){
-    finalZ += aux3 * 0.1;
-  }
-  if(i == 2){
-    finalZ += aux2 * 0.1 + aux3 * 0.01;
-  }
-  if(i == 3){
-    finalZ += aux1 * 0.1 + aux2 * 0.01 + aux3 * 0.001;
-  }
+  finalY += aux1 * 0.1 +  aux2 * 0.01 +  aux3 * 0.001 +  aux4 * 0.0001 +  aux5 * 0.00001;
 
 }
 
@@ -877,22 +995,13 @@ void coordenadaZ(int posC){ // Función para almacenar la coordenada Z
 
 void movimiento_PaP(){ // Supongamos base de 450mmx450mm
 
-  float diferencia1, diferencia2;
-  if(coordenadas[0] != coordenadasPrevias[0]){ // 25 pasos equivalen a 1mm
-    diferencia1 = coordenadas[0] - coordenadasPrevias[0];
-    myStepper1.step(diferencia1 * 25);
-  }
-  if(coordenadas[1] != coordenadasPrevias[1]){ // 25 pasos equivalen a 1mm
-    diferencia2 = coordenadas[0] - coordenadasPrevias[0];
-    myStepper2.step(diferencia2 * 25);
-  }
+  // Esta seria la función que tenes que hacer
   
 }
 
 void retorno_Al_Home(){
 
-  myStepper1.step(-coordenadas[0] * 25);
-  myStepper2.step(-coordenadas[1] * 25);
+  // Esta otra cuando tengas la logica de la de arriba tendria que ser muy facil y solo se ejecutaria al final de la secuencia
 
 }
 
@@ -905,5 +1014,31 @@ void interrupcion_Externa(){
 ////--------------------------------------Timer2--------------------------------------
 
 void timer(){
+
+  mil1++;
+  mil2++;
+  mil3++;
+  mil4++;
+  mil5++;
+  mil6++;
+  mil7++;
+  mil8++;
+  mil9++;
+  mil10++;
+  mil11++;
+  mil12++;
+  mil13++;
+  mil14++;
+  mil15++;
+  mil16++;
+  mil17++;
+  mil18++;
+  mil19++;
+  mil20++;
+  mil21++;
+  mil22++;
+  mil23++;
+  mil24++;
+  mil25++;
 
 }
